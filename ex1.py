a8b9190a612c15b6133a47acebd4741fffd020c7
Kali <samuel.lao85@gmail.com>
2022-05-10 01:36:06 -0400

    Second exercise...and some modifications to ex1.py

--- ex1.py
+++ ex1.py
@@ -5,5 +5,5 @@ print "This is fun."
 print "Yay! Printing."
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
-print "new line study drills taks"
+print "new line study drills tasks"
 
