cfc1bce503d4a2370a3a2a3c88dda1159fbd79bf
Kali <samuel.lao85@gmail.com>
2022-06-06 12:06:11 -0400

    Adding modifications on ex6.py

--- ex6.py
+++ ex6.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
+#Store string in  variable x with a modulus operation on number 10
 x = "There are %d types of people."  % 10
+
+#Store string 'binary' on binary variable
 binary = "binary"
+
+#Store string 'don't'
 do_not = "don't"
+
+#Store string with that shows 2 variable contents (binary and do_not)
 y = "Those who know %s and those who %s." % (binary, do_not)
 
 print x
@@ -11,7 +18,7 @@ print y
 print "I said: %r." % x
 print "I also said: '%s'." % y
 
-hilarious = False
+hilarious = True
 joke_evaluation = "Isn't that joke so funny?! %r"
 
 print joke_evaluation % hilarious
@@ -20,3 +27,5 @@ w = "This is the left side of..."
 e = "a string with a right side."
 
 print w + e
+
+#lines with strings: 16, 18, 19, 29 (missing line is 24)
