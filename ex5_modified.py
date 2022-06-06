e67b2fc3649c05dc70625a70a98d870b3d5b9f4c
Kali <samuel.lao85@gmail.com>
2022-06-06 00:00:43 -0400

    Modifications for ex5.py

--- ex5_modified.py
+++ ex5_modified.py
@@ -3,19 +3,29 @@
 name = 'Zed A. Shaw'
 age = 35 # Not a lie
 height = 74 # inches
-my_weight = 180 #lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+test_variable = "test"
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+#convert inches to cm
+convert_cm = height * 2.54
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+#Adding %r as formatting
+print "Test for string %r" % test_variable
+
+
+#Conversion of inches  to cm
+print "Conversion of height from inches to cm %d" % convert_cm
 
 #This line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
 
 
