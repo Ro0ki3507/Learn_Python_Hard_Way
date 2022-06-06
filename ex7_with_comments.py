aa032ba73929bcf3fd7a47e8da63c239213aae9c
Kali <samuel.lao85@gmail.com>
2022-06-06 17:31:59 -0400

    Adding modifications on ex7.py

---
+++ ex7_with_comments.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+#print some string
+print "Mary had a little lamb."
+
+#print some string and add an additional string format content
+print "Its fleece was white as %s." % 'snow'
+
+#print some string
+print "And everywhere that Mary went."
+
+#print some dots multiplied by 10
+print "." * 10 #What'd that do ?
+
+#Store Cheese burger string on each variable endx
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch that comma at the end. Try removing it to se what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
