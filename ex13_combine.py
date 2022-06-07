c92798a05efeada2487ca708842d7848bbb85651
Kali <samuel.lao85@gmail.com>
2022-06-07 16:38:50 -0400

    upload ex13.py and additional exercises

---
+++ ex13_combine.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from sys import argv
+
+script, first, second = argv
+
+print script, first, second
+
+info_user = raw_input("Insert the info required: ")
+
+print "Info inserted is %s" % info_user
+
+
---
+++ ex13_fewer_arg.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from sys import argv
+
+scriptname, first_v, second_v = argv
+
+print "The script is called:", scriptname
+print "Your first variable is:", first_v
+print "Your second variable is:", second_v
+
